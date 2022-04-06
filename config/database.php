8619ca0cdeea9f1e3e00f7b049148172585ef5c7
michael silverio <97074678+michaelsilverio@users.noreply.github.com>
2022-04-06 09:41:49 +0800

    Deployment (#19)
    * Deployment
    
    * Updated the app.js script file
    
    * Modified the app.js file
    
    * Added database.php
    
    * Added the app.js file

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('postgres://junxqamjzasrie:c32ae7762057535e73e2fb25ad1435791ab09c2f313302cd1895423988be08e5@ec2-54-173-77-184.compute-1.amazonaws.com:5432/dfu3v8ecd4at30');
+
 return [
 
     /*
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +67,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => 'postgres://junxqamjzasrie:c32ae7762057535e73e2fb25ad1435791ab09c2f313302cd1895423988be08e5@ec2-54-173-77-184.compute-1.amazonaws.com:5432/dfu3v8ecd4at30',
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
--- resources/js/config.js
+++ resources/js/config.js
@@ -1,3 +1,3 @@
 // App URL configurations
-export const BASE_URL = "http://127.0.0.1:8000";
-export const API_BASE_URL = "http://127.0.0.1:8000/api/v1";
+export const BASE_URL = process.env.MIX_BASE_URL;
+export const API_BASE_URL = `${BASE_URL}/api/v1`;
